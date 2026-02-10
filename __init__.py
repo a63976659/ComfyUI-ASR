0bac731a067644bfb28bd2a82456d6c642898a79
猪的飞行梦 <63976659@qq.com>
2026-02-10 16:07:38 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .节点.asr import Qwen语音识别_Node
+from .节点.asr import Qwen批量语音识别_Node
 
 NODE_CLASS_MAPPINGS = {
-    "Qwen语音识别": Qwen语音识别_Node
+    "Qwen语音识别": Qwen语音识别_Node,
+    "Qwen批量语音识别": Qwen批量语音识别_Node
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Qwen语音识别": "🎤 Qwen 语音识别 (ASR)"
+    "Qwen语音识别": "🎤 Qwen 语音识别 (ASR)",
+    "Qwen批量语音识别": "🎤 Qwen批量语音识别 (ASR)"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
